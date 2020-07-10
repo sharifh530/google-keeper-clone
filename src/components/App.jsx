cb350c54b9a580e67af2a522b35694041d7d91dc
Sazzad Hossain <sharifh530@gmail.com>
2020-07-11 03:23:24 +0600

    Keeper app 2nd part has been updated.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNote(notes) {
+  return <Note title={notes.title} content={notes.content} />;
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
--- src/notes.js
+++ src/notes.js
@@ -23,4 +23,18 @@ const notes = [
     content:
       "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
   },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
+  },
 ];
+
+export default notes;
